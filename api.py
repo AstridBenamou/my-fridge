b72cb3acaa0917503826e98dc8735d55c2abe2de
AstridBenamou <astrid.benamou@dauphine.eu>
2023-11-02 16:35:38 +0100

    link api to streamlit

--- api.py
+++ api.py
@@ -5,88 +5,51 @@ import json
 
 app = FastAPI()
 
-class Kitchen_item(BaseModel):
-    name: str
-    quantity: int
-
 with open('data/pantry.json', 'r') as file:
-    pantry_data = json.load(file)
+    data = json.load(file)
 
-
-class PantryItem(BaseModel):
-    name: str
-    quantity: int
-
-class PantryData(BaseModel):
-    vegetables: List[PantryItem]
-    condiments: List[PantryItem]
-    starches: List[PantryItem]
-    dairies: List[PantryItem]
-    tins: List[PantryItem]
-    others: List[PantryItem]
-
+class ItemRequest(BaseModel):
+    category: str
+    item_name: str
 
 @app.get("/pantry")
 async def get_pantry():
-    return pantry_data
-
-@app.post("/update_quantity")
-async def update_quantity(item: Kitchen_item):
-    global pantry_data
-    item_found = False
-    for category, items in pantry_data.items():
-        for i in range(len(items)):
-            if items[i]['name'] == item.name:
-                items[i]['quantity'] = item.quantity
-                item_found = True
-                break
-
-    if not item_found:
-        # Ajouter un nouvel élément si l'article n'existe pas déjà
-        pantry_data[category].append({"name": item.name, "quantity": item.quantity})
-
-    # Écrire les données mises à jour dans le fichier JSON
-    with open('data/pantry.json', 'w') as file:
-        json.dump(pantry_data, file, indent=4)
-
-    return {"message": f"La quantité pour {item.name} a été mise à jour à {item.quantity}."}
+    return data
 
 
-# @app.post("/increment_quantity")
-# async def increment_quantity(pantry_item: PantryItem):
-#     global pantry_data
-#     for category, items in pantry_data.items():
-#         for item in items:
-#             if item["name"] == pantry_item.name:
-#                 item["quantity"] += 1
-#                 break
+# Point de terminaison pour la requête POST
+@app.post('/increment_quantity')
+async def increment_quantity(item_request: ItemRequest):
+    category = item_request.category
+    item_name = item_request.item_name
 
-#     # Write the updated data to the JSON file
-#     try:
-#         with open('data/pantry.json', 'w') as file:
-#             json.dump(pantry_data, file, indent=4)
-#     except Exception as e:
-#         return {"message": f"Failed to update the JSON file: {e}"}
+    # Rechercher l'élément dans le fichier JSON et ajouter 1 à la quantité
+    for item in data[category]:
+        if item['name'] == item_name:
+            item['quantity'] += 1
 
-#     return {"message": f"Quantity of {pantry_item.name} incremented successfully."}
+    # Mettre à jour le fichier JSON
+    with open('data/pantry.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
-# @app.post("/decrement_quantity")
-# async def decrement_quantity(pantry_item: PantryItem):
-#     global pantry_data
-#     for category, items in pantry_data.items():
-#         for item in items:
-#             if item["name"] == pantry_item.name:
-#                 if item["quantity"] > 0:
-#                     item["quantity"] -= 1
-#                 else:
-#                     return {"message": f"The quantity of {pantry_item.name} is already 0."}
-#                 break
+    return {'message': f'La quantité de {item_name} a été mise à jour avec succès.'}
 
-#     # Write the updated data to the JSON file
-#     try:
-#         with open('data/pantry.json', 'w') as file:
-#             json.dump(pantry_data, file, indent=4)
-#     except Exception as e:
-#         return {"message": f"Failed to update the JSON file: {e}"}
 
-#     return {"message": f"Quantity of {pantry_item.name} decremented successfully."}
+# Point de terminaison pour la requête POST
+@app.post('/decrement_quantity')
+async def decrement_quantity(item_request: ItemRequest):
+    category = item_request.category
+    item_name = item_request.item_name
+
+    # Rechercher l'élément dans le fichier JSON et ajouter 1 à la quantité
+    for item in data[category]:
+        if item['name'] == item_name:
+            if item['quantity'] > 0:
+                item['quantity'] -= 1
+            break
+
+    # Mettre à jour le fichier JSON
+    with open('data/pantry.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+    return {'message': f'La quantité de {item_name} a été mise à jour avec succès.'}
--- kitchen.py
+++ kitchen.py
@@ -1,57 +1,47 @@
-# import streamlit as st
-# import pandas as pd 
-# import json
-
-# def show_kitchen():
-#     #categories = pd.read_csv("data/categories.csv")
-#     st.title("Kitchen")
-#     st.write("List of items available in my pantry and fridge:")
-
-#     with open('data/categories.json') as f:
-#         data = json.load(f)
-
-#     for category, items in data.items():
-#             st.subheader(category.capitalize())
-#             data_table = {"Items": items, "Quantities": [0] * len(items)}
-#             df = pd.DataFrame(data_table)
-#             st.dataframe(df, height=300, hide_index=True)
-
-
 import streamlit as st
 import pandas as pd
 import json
 import requests
 
-API_URL = "http://localhost:8000"  # Remplacez ceci par votre URL d'API
+def increment_quantity(category, item_name):
+    url = "http://localhost:8001/increment_quantity"
+    payload = {"category": category, "item_name": item_name}
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        st.write(f"Quantité mise à jour avec succès pour {item_name}.")
+    else:
+        st.write(f"Erreur lors de la mise à jour de la quantité pour {item_name}.")
+
+def decrement_quantity(category, item_name):
+    url = "http://localhost:8001/decrement_quantity"
+    payload = {"category": category, "item_name": item_name}
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        st.write(f"Quantité mise à jour avec succès pour {item_name}.")
+    else:
+        st.write(f"Erreur lors de la mise à jour de la quantité pour {item_name}.")
+
 
 def show_kitchen():
     st.title("Kitchen")
     st.write("List of items available in my pantry and fridge:")
 
-    with open('data/categories.json') as f:
+    with open('data/pantry.json') as f:
         data = json.load(f)
 
     for category, items in data.items():
         st.subheader(category.capitalize())
-        for item in items:
-            quantity = st.number_input(f"Enter quantity for {item}", value=0)
-            if st.button(f"Increment {item}"):
-                payload = {"name": item, "quantity": 1}
-                response = requests.post(f"{API_URL}/update_quantity", json=payload)
-                if response.status_code == 200:
-                    st.write(response.json()["message"])
-                else:
-                    st.write("Failed to increment quantity.")
-            # if st.button(f"Decrement {item}"):
-            #     payload = {"name": item, "quantity": 1}
-            #     response = requests.post(f"{API_URL}/decrement_quantity", json=payload)
-            #     if response.status_code == 200:
-            #         st.write(response.json()["message"])
-            #     else:
-            #         st.write("Failed to decrement quantity.")
+        df = pd.DataFrame(items)
 
-            # Afficher la quantité actuelle de l'élément
-            st.write(f"Current quantity: {quantity}")
+        col1, col2, col3 = st.columns(3)
 
-if __name__ == '__main__':
-    show_kitchen()
+        col1.table(df.set_index('name'))
+        for index, row in df.iterrows():
+            if col2.button(f"{row['name']} - ", use_container_width= True):
+                decrement_quantity(category, row['name']) 
+                st.write(f"You clicked the button for {row['name']}.")
+            if col3.button(f"{row['name']} + ", use_container_width= True):
+                increment_quantity(category, row['name'])
+                st.write(f"You clicked the button for {row['name']}.")
